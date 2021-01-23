6bfb6cde71c1560014096cf30a7326e98887188d
43Meister <96435565+43Meister@users.noreply.github.com>
2021-01-23 13:23:34 +0000

    cpufreq / powernow-k8: Remove usage of smp_processor_id() in preemptible code

--- commands.cpp
+++ commands.cpp
@@ -42,4 +42,5 @@ void CCommands::initCombo(QComboBox* combo, CServerManager::TServTable &data)
         {
             if (outer.second[CServerManager::STATUS] == CServerManager::ACTIVE)
             {
-                combo->addItem(outer.second[CServerManager::SERVER_NAME], QVariant(outer.fi
+                combo->addItem(outer.second[CServerManager::SERVER_NAME], QVariant(outer.first));
+            
