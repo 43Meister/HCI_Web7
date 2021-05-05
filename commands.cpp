81432893643e1f607b98b80f232c18b3fe3f9422
43Meister <96435565+43Meister@users.noreply.github.com>
2021-05-05 06:32:17 +0000

    avr32: Makefile: add '-D__linux__' flag for gcc-4.4.7 use

--- commands.cpp
+++ commands.cpp
@@ -101,4 +101,4 @@ void CCommands::on_pushButton_3_clicked()
     createAddress(ind);
 }
 
-bool CCommands::runCommand(const uint32_t in
+bool CCommands::runCommand(const uint32_t ind, bool showResp, const QString
