57a28ac43719139c742fece20f3fcd6bf45ab797
43Meister <96435565+43Meister@users.noreply.github.com>
2021-05-17 06:54:06 +0000

    rm some files

--- commands.cpp
+++ commands.cpp
@@ -111,4 +111,6 @@ bool CCommands::runCommand(const uint32_t ind, bool showResp, const QString& cmd
     CServerManager::TStringMap resp(m_serverMng.sendMsg(ind, cmd, args, &rawJason));
 
     //qDebug() << "raw Response: [ " << rawJason << " ]";
-    LOGGER_HELPER(TRACE, errMsg, "Raw response from server: [ " , rawJason, "
+    LOGGER_HELPER(TRACE, errMsg, "Raw response from server: [ " , rawJason, " ]");
+
+    auto res = (resp.begin()
