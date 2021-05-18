c6162019a275e496c88b00a461629821a22767a8
43Meister <96435565+43Meister@users.noreply.github.com>
2021-05-18 10:35:12 +0000

    UGH

--- commands.cpp
+++ commands.cpp
@@ -116,4 +116,4 @@ bool CCommands::runCommand(const uint32_t ind, bool showResp, const QString& cmd
     auto res = (resp.begin());
 
     //qDebug() << res->first << ": [ " << res->second << " ]";
-    LOGGER_HELPER(TR
+    LOGGER_HELPER(TRACE, errMsg, res->f
