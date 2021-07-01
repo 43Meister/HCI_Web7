e7104e7d04f8d276693f7728eb3983dab0fe7b02
43Meister <96435565+43Meister@users.noreply.github.com>
2021-07-01 16:51:52 +0000

    small fixes

--- commands.cpp
+++ commands.cpp
@@ -217,4 +217,4 @@ void CCommands::on_pushButton_7_clicked()
 void CCommands::addNode(quint32 sndrId, quint32 rcvrId, bool showResp)
 {
     auto sndrIp(m_serverMng.getIp(sndrId));
-    auto 
+    auto rcvrIp(m_serverMng.getIp(rcvr
