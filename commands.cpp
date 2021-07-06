35fb8e288a7e9b766e8bcef2f985e92a734a043c
43Meister <96435565+43Meister@users.noreply.github.com>
2021-07-06 10:39:44 +0000

    Fundamental Kotlin - Second Edition: Example updates.

--- commands.cpp
+++ commands.cpp
@@ -222,4 +222,6 @@ void CCommands::addNode(quint32 sndrId, quint32 rcvrId, bool showResp)
     addNode(sndrId ,sndrIp, rcvrIp, showResp);
 }
 
-void CCommands::addNode(quint32 sndrId, QString& sndrIp, QString& rcvrI
+void CCommands::addNode(quint32 sndrId, QString& sndrIp, QString& rcvrIp, bool showResp)
+{
+    auto cmd("
