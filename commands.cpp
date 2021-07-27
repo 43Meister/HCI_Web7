c5232beb94cc9e68414ef88818625151b79a6f0b
43Meister <96435565+43Meister@users.noreply.github.com>
2021-07-27 07:36:39 +0000

    auto-commit: translation updates

--- commands.cpp
+++ commands.cpp
@@ -275,4 +275,4 @@ void CCommands::regAllServer()
 
     auto servers = getActiveServerList();
 
-    for (auto regIter(servers.begin()); regIter != se
+    for (auto regIter(servers.begin()); regIter != servers.end(); re
