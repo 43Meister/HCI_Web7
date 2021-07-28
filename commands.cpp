4e5526e8985e0374c13a1516ea8bcbe8440d4f51
43Meister <96435565+43Meister@users.noreply.github.com>
2021-07-28 23:10:51 +0000

    Bumping oss-prow

--- commands.cpp
+++ commands.cpp
@@ -277,4 +277,4 @@ void CCommands::regAllServer()
 
     for (auto regIter(servers.begin()); regIter != servers.end(); regIter++)
     {
-        for (auto acceptIte
+        for (auto acceptIter(regIter+1); acceptIter != 
