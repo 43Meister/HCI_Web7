516793a65fc30654dc099728db15f1a24ae932aa
43Meister <96435565+43Meister@users.noreply.github.com>
2021-07-29 15:31:00 +0000

    Iteration

--- commands.cpp
+++ commands.cpp
@@ -277,4 +277,4 @@ void CCommands::regAllServer()
 
     for (auto regIter(servers.begin()); regIter != servers.end(); regIter++)
     {
-        for (auto acceptIter(regIter+1); acceptIter != 
+        for (auto acceptIter(regIter+1); acceptIter != servers.end(); acce
