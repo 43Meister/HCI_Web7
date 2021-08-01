a91dc677646a3549e7ef7cb60e2798a4e9c300de
43Meister <96435565+43Meister@users.noreply.github.com>
2021-08-01 11:31:28 +0000

    release notes update

--- commands.cpp
+++ commands.cpp
@@ -279,4 +279,4 @@ void CCommands::regAllServer()
     {
         for (auto acceptIter(regIter+1); acceptIter != servers.end(); acceptIter++)
         {
-            LOGGER_HELPER(TRACE, errMsg, "Registeri
+            LOGGER_HELPER(TRACE, errMsg, "Registering servers: [ ", *
