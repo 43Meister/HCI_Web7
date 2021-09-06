2df269bad26868b4fadb74d9c2fe4897eecbe509
43Meister <96435565+43Meister@users.noreply.github.com>
2021-09-06 20:42:45 +0000

    updated hebrew translation

--- commands.h
+++ commands.h
@@ -1,3 +1,28 @@
 
 #ifndef COMMANDS_H
 #define COMMANDS_H
+
+#include <QDialog>
+#include "servermanager.h"
+#include "showresp.h"
+#include <utility>
+#include <iostream>
+#include <QComboBox>
+#include <logable.h>
+#include <QVector>
+
+namespace Ui {
+class CCommands;
+}
+
+class CCommands : public QDialog, CLogable
+{
+
+    //this is my friend
+    friend class CDemo;
+
+    Q_OBJECT
+
+public:
+    explicit CCommands(QWidget *parent = 0);
+    ~CCommands();
