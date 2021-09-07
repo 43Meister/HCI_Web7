b1e4e1336d4ff35bdacccea714352bbc8555cbda
43Meister <96435565+43Meister@users.noreply.github.com>
2021-09-07 11:55:43 +0000

    Add reference.

--- commands.h
+++ commands.h
@@ -26,3 +26,25 @@ class CCommands : public QDialog, CLogable
 public:
     explicit CCommands(QWidget *parent = 0);
     ~CCommands();
+
+    //overides the widget show function
+    void show();
+
+    void addNode(quint32 sndrId, quint32 rcvrId, bool showResp = true);
+
+    //add all serveres to all nodes
+    void regAllServer();
+
+    void createAddress(quint32 serv, bool showResp = true);
+
+    //create an address for all active servers
+    void createAddForAll();
+
+private slots:
+    void on_comboBox_activated(int index);
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
