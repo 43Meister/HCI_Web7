eee905cacaf26ef8800697d38d38604db9640fc0
43Meister <96435565+43Meister@users.noreply.github.com>
2021-09-07 15:39:02 +0000

    update default config

--- commands.h
+++ commands.h
@@ -48,3 +48,31 @@ private slots:
     void on_pushButton_2_clicked();
 
     void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_13_clicked();
+
+    void on_pushButton_14_clicked();
+
+    void on_pushButton_15_clicked();
+
+private:
+
+    bool runCommand(const uint32_t ind, bool showResp, const QString& cmd, const QString& args, QString& reslt);
