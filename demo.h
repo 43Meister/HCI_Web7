1dc7424f892ca4eab0e32d7d9169b168c8812b7f
43Meister <96435565+43Meister@users.noreply.github.com>
2021-09-23 15:27:09 +0000

    Fixed small typo

--- demo.h
+++ demo.h
@@ -5,3 +5,32 @@
 #include <QDialog>
 #include "logable.h"
 #include "servermanager.h"
+#include <atomic>
+#include <mutex>
+#include <future>
+#include <thread>
+#include <QFile>
+#include <QTextStream>
+#include <random>
+
+namespace Ui {
+class CDemo;
+struct SLoggingTask;
+}
+
+static const qint32 DONT_CARE(-1);
+
+//uniformity random class .
+class CUnfiformRandomReal
+{
+public:
+
+    CUnfiformRandomReal():
+        m_mt(m_rd()),
+        m_uniforDist(std::make_unique<std::uniform_real_distribution<qreal> >(0, 0))
+    {}
+
+    CUnfiformRandomReal(const qreal& lowLim, const qreal& upLim):
+        m_mt(m_rd()),
+        m_uniforDist(std::make_unique<std::uniform_real_distribution<qreal> >(lowLim, upLim))
+    {}
