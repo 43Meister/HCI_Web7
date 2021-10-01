fec5c2974a5876837865b1d9fce0abc4f9f8607f
43Meister <96435565+43Meister@users.noreply.github.com>
2021-10-01 15:58:42 +0000

    Unused import.

--- logable.cpp
+++ logable.cpp
@@ -1,4 +1,5 @@
 #include "logable.h"
 
 CLogable::CLogable(QString logName) :
-    m_logger(log4cxx::Logger::getLogger(logName.toStd
+    m_logger(log4cxx::Logger::getLogger(logName.toStdString()))
+{}
