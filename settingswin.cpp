4d314718b80b81efa88c602b25bab1c96d7c70a0
43Meister <96435565+43Meister@users.noreply.github.com>
2022-02-10 07:22:46 +0000

    Update mysql.php

--- settingswin.cpp
+++ settingswin.cpp
@@ -1,3 +1,23 @@
 
 #include "settingswin.h"
 #include "ui_settingswin.h"
+#include "qdebug.h"
+#include "servermanager.h"
+
+CSettingsWin::CSettingsWin(QWidget *parent) :
+    QDialog(parent),
+    CLogable("SettingDialogLog"),
+    ui(new Ui::CSettingsWin)
+{
+    ui->setupUi(this);
+}
+
+CSettingsWin::~CSettingsWin()
+{
+    delete ui;
+}
+
+void CSettingsWin::on_buttonBox_accepted()
+{
+    int numOfDockers = ((ui->lineEdit)->text()).toInt();
+    std::string errMsg("");
