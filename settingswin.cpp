b5426d458dd232305bfd48601094c5b48b301760
43Meister <96435565+43Meister@users.noreply.github.com>
2022-02-12 08:30:39 +0000

    Q3Frame--

--- settingswin.cpp
+++ settingswin.cpp
@@ -21,3 +21,24 @@ void CSettingsWin::on_buttonBox_accepted()
 {
     int numOfDockers = ((ui->lineEdit)->text()).toInt();
     std::string errMsg("");
+
+    for (int docker(0); docker < numOfDockers; ++docker)
+    {
+        CServerManager::getReference().registerClient();
+    }
+
+    //qDebug() << "Created: " << numOfDockers << " Servers";
+    LOGGER_HELPER(DEBUG, errMsg, "Created: " , numOfDockers, " Servers");
+
+}
+
+void CSettingsWin::on_buttonBox_rejected()
+{
+    std::string errMsg("");
+    LOGGER_HELPER(DEBUG, errMsg, "Oper canceled");
+
+}
+
+void CSettingsWin::on_lineEdit_editingFinished()
+{
+    m_numOfDockers = ((ui->lineEdit)->text()).toInt();
