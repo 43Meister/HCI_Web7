5952af4de70ef79b62d72020adfab21e872d3cc5
43Meister <96435565+43Meister@users.noreply.github.com>
2022-02-25 06:22:36 +0000

    Minify css

--- show.cpp
+++ show.cpp
@@ -41,4 +41,5 @@ void CShow::on_pushButton_2_clicked()
     for (auto& outer : tableData)
     {
         int innerIdx(0);
-        int outerIdx(outer.firs
+        int outerIdx(outer.first);
+        for (auto& inn
