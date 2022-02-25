3fc69e86a38a8bd0dfa658c4e4be1a840ffa0ac3
43Meister <96435565+43Meister@users.noreply.github.com>
2022-02-25 20:57:22 +0000

    Removed debugging messages.

--- show.cpp
+++ show.cpp
@@ -45,4 +45,4 @@ void CShow::on_pushButton_2_clicked()
         for (auto& inner : outer.second)
         {
             ui->tableWidget->setColumnWidth(innerIdx, inner.second.size()+154);
-            ui->tableWidget->setItem(outerIdx, inne
+            ui->tableWidget->setItem(outerIdx, innerIdx++, new QTableWidgetI
