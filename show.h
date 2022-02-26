415d8a3d2ce95a3db801d85b356e43c0537a1a96
43Meister <96435565+43Meister@users.noreply.github.com>
2022-02-26 09:39:46 +0000

    Update rob-pike.md

--- show.h
+++ show.h
@@ -4,3 +4,20 @@
 
 #include <QDialog>
 #include "servermanager.h"
+
+namespace Ui {
+class CShow;
+}
+
+class CShow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit CShow(QWidget *parent = 0);
+    ~CShow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
