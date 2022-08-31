24254180a42212bc0f98a323636c0a9c63aec36a
43Meister <96435565+43Meister@users.noreply.github.com>
2022-08-31 21:38:09 +0000

    merge into master

--- startservers.cpp
+++ startservers.cpp
@@ -32,4 +32,5 @@ void CStartServers::show()
         {
             //qDebug() << "the status is: " <<  outer.second["Status"];
             LOGGER_HELPER(DEBUG, errMsg, QString("the status is "),  outer.second["Status"]);
-            if (outer.second[CServerManager::STATUS] == CS
+            if (outer.second[CServerManager::STATUS] == CServerManager::DEACTIVE)
+ 
