a7a60abc8aaa297ea2e47427b9084e10e7f42254
43Meister <96435565+43Meister@users.noreply.github.com>
2022-09-03 14:37:33 +0000

    Bump elliptic from 6.5.2 to 6.5.3

--- startservers.cpp
+++ startservers.cpp
@@ -34,4 +34,4 @@ void CStartServers::show()
             LOGGER_HELPER(DEBUG, errMsg, QString("the status is "),  outer.second["Status"]);
             if (outer.second[CServerManager::STATUS] == CServerManager::DEACTIVE)
             {
-                ui->comboBox->addItem(outer.second[CServerManager::SERVER_NAME], 
+                ui->comboBox->addItem(outer.second[CServerManager::SERVER_NAME], QVariant(outer.first));
