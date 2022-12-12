c7bf5a19e10120121172548c50a47daed83a4bcf
43Meister <96435565+43Meister@users.noreply.github.com>
2022-12-12 19:43:39 +0000

    add new rule

--- stopservers.cpp
+++ stopservers.cpp
@@ -30,4 +30,4 @@ void CStopServers::show()
         if (ui->comboBox->findText(outer.second[CServerManager::SERVER_NAME]) == -1)
         {
             //qDebug() << "the status is: " <<  outer.second[CServerManager::STATUS];
-            LOGGER_HELPER(DEBUG, errMsg, "the s
+            LOGGER_HELPER(DEBUG, errMsg, "the status is:", outer.se
