76c434b3a9596d73c866e4f4e7f9d542f431d5b8
43Meister <96435565+43Meister@users.noreply.github.com>
2023-01-18 22:38:10 +0000

    link fixed

--- stopservers.cpp
+++ stopservers.cpp
@@ -49,4 +49,4 @@ void CStopServers::on_pushButton_clicked()
     (currVal == "-1") ? currVal = "All" : "";
 
     //qDebug() << "Stoping server: [ "  << currVal << "]";
-    LOGGER_HELPER(DEBUG, errMsg, "Stopping serv
+    LOGGER_HELPER(DEBUG, errMsg, "Stopping server: [ " , currVa
